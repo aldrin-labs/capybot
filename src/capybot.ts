28c57d5249dcac7795890997b949a19e2cb143c6
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2024-04-01 22:38:50 +0100

    Log pool state and imb. ratios in bot's main loop

--- src/capybot.ts
+++ src/capybot.ts
@@ -6,11 +6,12 @@ import { setTimeout } from 'timers/promises'
 import { DataSource } from './data_sources/data_source'
 import { SuiNetworks, getFullnodeUrl } from './networks'
 import { CetusPool } from './dexs/cetus/cetus'
-import { CetusParams, RAMMSuiParams, TurbosParams } from './dexs/dexsParams'
+import { CetusParams, RAMMSuiParams } from './dexs/dexsParams'
 import { Pool } from './dexs/pool'
 import { logger } from './logger'
 import { Strategy } from './strategies/strategy'
 import { RAMMPool } from './dexs/ramm-sui/ramm-sui'
+import { processImbRatioEvent, processPoolStateEvent } from '@ramm/ramm-sui-sdk'
 
 // Default gas budget: 0.5 `SUI`
 const DEFAULT_GAS_BUDGET: number = 0.5 * (10 ** 9)
@@ -26,17 +27,22 @@ export class Capybot {
      *
      * The key is the UUID (a.k.a. URI) of the pool, and the value is the pool object.
      */
-    public pools: Record<
-        string,
-        Pool<CetusParams | TurbosParams | RAMMSuiParams>
-    > = {}
-
+    public pools: Record<string, Pool<CetusParams | RAMMSuiParams>> = {}
     /**
      * A record of the keypairs each pool uses to sign its transactions.
      *
-     * The key is the address of the pool, and the value is the keypair object.
+     * The key is the UUID of the pool, and the value is the keypair object.
      */
     public poolKeypairs: Record<string, Keypair> = {}
+
+    /**
+     * A record of all the RAMM pools the bot is subscribed to.
+     * Each pool, regardless of how many trading pairs or strategies it is a part of, is represented by a single
+     * entry here.
+     *
+     * The key is the Sui address of the pool.
+     */
+    public rammPools: Record<string, RAMMPool> = {}
 
     public strategies: Record<string, Array<Strategy>> = {}
     private suiClient: SuiClient
@@ -137,6 +143,32 @@ export class Capybot {
                     }
                 }
             }
+
+            for (const rammAddress in this.rammPools) {
+                const rammPool = this.rammPools[rammAddress]
+                const rammKeypair = this.poolKeypairs[rammPool.uri]
+
+                const { poolStateEventJSON, imbRatioEventJSON } =
+                    await rammPool.rammSuiPool.getPoolStateAndImbalanceRatios(
+                        rammPool.suiClient,
+                        rammKeypair.toSuiAddress()
+                    )
+
+                const poolState = processPoolStateEvent(rammPool.rammSuiPool, poolStateEventJSON)
+                const imbRatioData = processImbRatioEvent(rammPool.rammSuiPool, imbRatioEventJSON)
+
+                logger.info(
+                    { ramm_id: poolState.rammID, data: poolState.assetBalances },
+                    'ramm pool state'
+                )
+
+                logger.info(
+                    { ramm_id: imbRatioData.rammID, data: imbRatioData.imbRatios},
+                    'imb ratios'
+                )
+            }
+
+
             await setTimeout(delay)
         }
     }
@@ -194,7 +226,7 @@ export class Capybot {
 
     /** Add a new pool for this bot to use for trading. */
     addPool(
-        pool: Pool<CetusParams | RAMMSuiParams | TurbosParams>,
+        pool: Pool<CetusParams | RAMMSuiParams>,
         keypair: Keypair
     ) {
         if (this.pools.hasOwnProperty(pool.uuid)) {
@@ -206,6 +238,12 @@ export class Capybot {
                 '/' +
                 pool.coinB.type
             throw new Error(errMsg)
+        }
+
+        // If the pool being added is a RAMM, and has not yet been recorded by the bot, take
+        // note of it.
+        if (pool instanceof RAMMPool && !this.rammPools.hasOwnProperty(pool.uri)) {
+            this.rammPools[pool.address] = pool as RAMMPool
         }
 
         this.pools[pool.uri] = pool
