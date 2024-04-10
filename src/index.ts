54ac29617563c9d1df7da8004be3b7b6295e16ef
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2024-04-10 14:21:26 +0100

    Halve default trading amounts

--- src/index.ts
+++ src/index.ts
@@ -19,12 +19,12 @@ const ARBITRAGE_RELATIVE_LIMIT = 1.001
 //const MARKET_DIFFERENCE_LIMIT = 1.01
 
 /**
- * Default amount to trade, for each token. Set to approximately 0.1 USD each.
+ * Default amount to trade, for each token. Set to approximately 0.05 USD each.
  */
 export const defaultAmounts: Record<string, number> = {}
-defaultAmounts[Assets.SUI.type] = 0.06
-defaultAmounts[Assets.USDC.type] = 0.1
-defaultAmounts[Assets.USDT.type] = 0.1
+defaultAmounts[Assets.SUI.type] = 0.034
+defaultAmounts[Assets.USDC.type] = 0.05
+defaultAmounts[Assets.USDT.type] = 0.05
 
 // Setup wallet from passphrase.
 const cetusUsdcSuiPhrase = process.env.CETUS_SUI_USDC_ADMIN_PHRASE
